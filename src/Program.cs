d4323862a58651af0142ed8b85e21aca27533268
Pere <perepicomuntaner@gmail.com>
2022-11-18 19:33:07 +0100

    Port to .NET 7.0

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SudokuKata.Domain;
+
+var solver = new SudokuSolver();
+
+solver.Play();
+
+Console.WriteLine();
+Console.Write("Press ENTER to exit... ");
+Console.ReadLine();
